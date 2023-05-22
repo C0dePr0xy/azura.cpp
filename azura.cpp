e7a56072de86f47082d5266ab9860a6429139aad
C0dePr0xy <87400651+C0dePr0xy@users.noreply.github.com>
2023-05-22 14:38:45 -0400

    Add files via upload

---
+++ azura.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+int main()
+{
+    std::string uname = "";
+    std::string pword = "";
+    std::string yesno;
+    int numkey;
+
+    std::cout << "PROJECT AZURA\n";
+    std::cout << " \n";
+    std::cout << "Enter Valid Username:\n";
+    std::cin >> uname;
+    std::cout << "Hello " << uname << " welcome to PROJECT AZURA\n";
+    std::cout << "Hello " << uname << ", please enter password:";
+    std::cin >> pword;
+    if (pword == "admin"){
+        std::cout << "Access Granted!\n";
+        std::cout << "Welcome " << uname << " to ASURA, Your personal planner.\n";
+        std::cout << "Just as a reminder, you can always edit goals in the code using a text editor of your choice! :D\n";
+    } else{
+        std::cout << "Access Denied!\n";
+        std::cout << uname << " is not permitted to use this application!\n";
+        std::cout << "Please close and re-open terminal to restart.\n";
+        std::cout << "Bye bye!! :3";
+    }
+    std::cout << "Would you like to continue?";
+    std::cin >> yesno;
+    if (yesno == "Y"){
+        std::cout << "Select which step 1-10 you want to do first please...\n";
+        std::cin >> numkey;
+        switch (numkey) {
+            case 1:
+                std::cout << "Brainstorm Ideas\n";
+                break;
+            case 2:
+                std::cout << "Organize Ideas\n";
+                break;
+            case 3:
+                std::cout << "Learn more C++\n";
+                break;
+            case 4:
+                std::cout << "Learn Blender3D\n";
+                break;
+            case 5:
+                std::cout << "Look for suitable game engine\n";
+                break;
+            case 6:
+                std::cout << "Install game engine\n";
+                break;
+            case 7:
+                std::cout << "Learn game engine\n";
+                break;
+            case 8:
+                std::cout << "Make simple character controller\n";
+                break;
+            case 9:
+                std::cout << "Design simple level\n";
+                break;
+            case 10:
+                std::cout << "Practice C++ more\n";
+                break;
+            default:
+                std::cout << "Invalid numkey!\n";
+                break;
+        }
+    }else if (yesno == "N"){
+        std::cout << "logging out...\n";
+    } else{
+        std::cout << "INVALID INPUT!! " << uname << " logging out, " << (pword == "CLEARED") << "EXITING...";
+    }
+}
