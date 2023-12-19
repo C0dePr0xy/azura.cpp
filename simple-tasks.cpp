835d89969fa0f7496e016410d8f5055a6f256b63
Eric <guerraeric495@gmail.com>
2023-12-19 13:04:36 -0500

    Update simple-tasks.cpp

--- simple-tasks.cpp
+++ simple-tasks.cpp
@@ -1,72 +1,89 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-using namespace std;
 
-char input;
 char confirmation;
 
 int main() {
-    while (true) {
-        std::cout << "Simple-Tasks for WINDOWS OS!\n";
+    char input;
 
+    while (true) {
+        std::cout << "\n";
+        std::cout << "\n";
+        std::cout << "Simple-Tasks for WINDOWS OS!\n";
         std::cout << "New Task [N]\n";
         std::cout << "Edit Task [E]\n";
         std::cout << "Delete Task [D]\n";
         std::cout << "List Tasks [L]\n";
         std::cout << "About [A]\n";
         std::cout << "Exit [X]\n";
-        std::cout << "\n";
         std::cin >> input;
         std::cout << "\n";
         std::cout << "\n";
 
-        
-        std::ofstream TaskFile ("Tasks.utsk");
+        std::ofstream TaskFile;
         std::string taskstring;
 
         switch (input) {
         case 'N':
-            TaskFile.open("Tasks.utsk");
+            TaskFile.open("Tasks.utsk", std::ios::app);
+            if (!TaskFile.is_open()) {
+                std::cerr << "Error opening Tasks.utsk for writing!\n";
+                return 1;
+            }
+            TaskFile.close();
             break;
         case 'E':
             TaskFile.open("Tasks.utsk", std::ios::out);
-            std::cout << "Enter TaskDetails: ";
-            std::cin >> taskstring;
+            if (!TaskFile.is_open()) {
+                std::cerr << "Error opening Tasks.utsk for writing!\n";
+                return 1;
+            }
+            std::cout << "Enter Task Details: ";
+            std::cin.ignore(); // Ignore newline character from previous input
+            std::getline(std::cin, taskstring);
             TaskFile << taskstring << "\n";
+            TaskFile.close();
             break;
+
         case 'D':
-            std::cout << "Deletion of Tasks.utsk is irreversable!\n";
+            std::cout << "Deletion of Tasks.utsk is irreversible!\n";
             std::cout << "Proceed? [Y] [N]\n";
             std::cin >> confirmation;
             if (confirmation == 'Y') {
-                remove("Tasks.utsk");
-                std::cout << "\n";
-                std::cout << "Tasks File Deletion Complete!\n";
-                std::cout << "\n";
+                if (std::remove("Tasks.utsk") == 0) {
+                    std::cout << "\n";
+                    std::cout << "\nTasks File Deletion Complete!\n";
+                    std::cout << "\n";
+                }
+                else {
+                    std::cout << "\n";
+                    std::cout << "Operation failed: Error Code: UEII03D!\n";
+                    std::cerr << "Error deleting Tasks.utsk!\n";
+                    std::cout << "\n";
+                }
             }
             else if (confirmation == 'N') {
-                std::cout << "Operation failed: \n";
-                std::cout << "Error Code: UEII01D!\n";
-                std::cout << "Refer to " << "MANUAL.txt" << " for more info\n";
+                std::cout << "\n";
+                std::cout << "Operation failed: Error Code: UEII01D!\n";
+                std::cout << "Refer to MANUAL.txt for more info\n";
+                std::cout << "\n";
             }
             else {
-                std::cout << "Invalid Input!\n";
-                std::cout << "Error Code: UEII02D!\n";
-                std::cout << "Refer to " << "MANUAL.txt" << " for more info\n";
+                std::cout << "\n";
+                std::cout << "Invalid Input! Error Code: UEII02D!\n";
+                std::cout << "\n";
+                std::cout << "Refer to MANUAL.txt for more info\n";
+                std::cout << "\n";
             }
             break;
         case 'L':
-            std::cout << "\n";
-            std::cout << "Feature in progress.\n";
-            std::cout << "\n";
+            std::cout << "\nFeature in progress.\n\n";
             break;
         case 'X':
             std::cout << "\n";
             std::cout << "Exiting program. Goodbye!\n";
             std::cout << "\n";
-            std::cout << "\n";
-            TaskFile.close();
             return 0;
         case 'A':
             std::cout << "(release) (r-v0.3)\n";
@@ -83,9 +100,8 @@ int main() {
             break;
         default:
             std::cout << "\n";
-            std::cout << "Invalid Input!\n";
-            std::cout << "Error Code: UEII02Q!\n";
-            std::cout << "Refer to " << "MANUAL.txt" << " for more info\n";
+            std::cout << "Invalid Input! Error Code: UEII02Q!\n";
+            std::cout << "Refer to MANUAL.txt for more info\n";
             std::cout << "\n";
             break;
         }
