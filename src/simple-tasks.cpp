918f74076189e563b2369aa29980d9513ec5df8b
Eric Guerra <87400651+C0dePr0xy@users.noreply.github.com>
2025-03-26 11:06:14 -0400

    bugfixing task creation feature.
    Signed-off-by: Eric Guerra <87400651+C0dePr0xy@users.noreply.github.com>

--- src/simple-tasks.cpp
+++ src/simple-tasks.cpp
@@ -85,7 +85,6 @@ void mainMenu()
             std::cout << "Task Description: ";
             std::cin.ignore();
             std::getline(std::cin, Task.taskDescription);
-            std::cin >> Task.taskDescription;
             taskFile.open(Task.taskName + ".task");
             taskFile << "[Name] " << Task.taskName << "\n";
             taskFile << "[Description] " << Task.taskDescription << "\n\n";
