44723f7f72a55fdd465c754d25cea671813b7aed
Eric Guerra <87400651+C0dePr0xy@users.noreply.github.com>
2025-03-27 13:19:32 -0400

    Revert simple-tasks.cpp

--- src/simple-tasks.cpp
+++ src/simple-tasks.cpp
@@ -84,10 +84,9 @@ void mainMenu()
             std::cout << "Task Name: ";
             std::cin >> Task.taskName;
             std::getline(std::cin, Task.taskName);
-            std::cin.ignore();
             std::cout << "Task Description: ";
-            std::getline(std::cin, Task.taskDescription);
             std::cin.ignore();
+            std::getline(std::cin, Task.taskDescription);
             taskFile.open(Task.taskName + ".task");
             taskFile << "[Name] " << Task.taskName << "\n";
             taskFile << "[Description] " << Task.taskDescription << "\n\n";
